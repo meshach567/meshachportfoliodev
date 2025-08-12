8603f4f1dab72eac4e3a83397ef7452954df33b4
Meshach Arinze <arinzemeshach123@gmail.com>
2025-08-12 14:09:21 +0100

    refactor: import lazysection, services, and and footer

--- app/page.tsx
+++ app/page.tsx
@@ -4,18 +4,31 @@ import { Projects } from "@/components/shared/Projects";
 import Skills from "@/components/shared/Skills";
 import Contact from "@/components/shared/contact";
 import Experience from "@/components/shared/Experience";
-
+import LazySection from "@/components/shared/LazySection";
+import Services from "@/components/shared/Services";
+import Footer from "@/components/shared/Footer";
 
 export default function Home() {
-
   return (
     <main>
       <Navbar />
       <HeroSection />
-      <Projects />
-      <Skills />
-      <Experience />
-      <Contact />
+      <LazySection>
+        <Projects />
+      </LazySection>
+      <LazySection>
+        <Skills />
+      </LazySection>
+      <LazySection>
+        <Experience />
+      </LazySection>
+      <LazySection>
+        <Services />
+      </LazySection>
+      <LazySection>
+        <Contact />
+      </LazySection>
+      <Footer />
     </main>
   );
 }
