dc1ea723f58e0a12d9f6d973be245981508bf7a4
Meshach Arinze <arinzemeshach123@gmail.com>
2025-08-13 17:56:06 +0100

    refactor: change the structure for naviation link

--- constants/navigation.ts
+++ constants/navigation.ts
@@ -1,28 +1,20 @@
 // Define the navigation items
 export const navigationOnLargeScreen = [
   { name: "Home", href: "/" },
-  { name: "Projects", href: "/projects" },
   { name: "Skills", href: "/#skills" },
   { name: "Experience", href: "/#experience" },
+  { name: "Contact", href: "/#contact" },
+  { name: "Projects", href: "/projects" },
   { name: "About", href: "/about" },
   { name: "Services", href: "/services" },
-  {
-    name: "Resume",
-    href: "",
-  },
-  { name: "Contact", href: "/#contact" },
 ];
 
 export const navigationOnMobile = [
   { name: "Home", href: "/" },
-  { name: "Projects", href: "/projects" },
   { name: "Skills", href: "/#skills" },
   { name: "Experience", href: "/#experience" },
+  { name: "Contact", href: "/#contact" },
+  { name: "Projects", href: "/projects" },
   { name: "About", href: "/about" },
   { name: "Services", href: "/services" },
-  {
-    name: "Resume",
-    href: "",
-  },
-  { name: "Contact", href: "/#contact" },
 ];
