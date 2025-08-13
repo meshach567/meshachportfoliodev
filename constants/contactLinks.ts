ab5b5936f4b859a61f4f0c503ace3155265f5cf8
Meshach Arinze <arinzemeshach123@gmail.com>
2025-08-13 18:05:57 +0100

    feat: add instragram and facebook icon

--- constants/contactLinks.ts
+++ constants/contactLinks.ts
@@ -4,4 +4,14 @@ export const contactlinks = [
     icon: "https://framerusercontent.com/images/RYuwqCxHlxrSfIOO7JZreY9YX9Q.png",
     href: "https://www.linkedin.com/in/meshach-ekene/",
   },
+  {
+    title: "GitHub",
+    icon: "https://framerusercontent.com/images/1b2d3c4e5f6g7h8i9j0k.png",
+    href: "https://github.com/meshach567"
+  },
+  {
+    title: "Facebook",
+    icon: "https://framerusercontent.com/images/1a2b3c4d5e6f7g8h9i0j.png",
+    href: "https://www.facebook.com/arinze.meshachekene"
+  }
 ];
